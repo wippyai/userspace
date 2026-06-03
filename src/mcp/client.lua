c4c607bc100939a11445f9b407dd204ae93011b9
Wolfy-J <wolfy.jd@gmail.com>
2026-06-03 16:42:57 -0400

    mcp(client): cast service_name for process.send; version 0.4.4

--- src/mcp/client.lua
+++ src/mcp/client.lua
@@ -57,7 +57,7 @@ local function send_and_wait(client, request_data, timeout_ms)
         reply_to_topic = request_data.reply_to_topic
     })
 
-    local send_ok = process.send(client.service_name, mcp_consts.TOPICS.REQUEST, request_data)
+    local send_ok = process.send(client.service_name :: string, mcp_consts.TOPICS.REQUEST, request_data)
     if not send_ok then
         client.log:error("Failed to send request", {
             service_name = client.service_name,
